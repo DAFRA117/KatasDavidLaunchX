4a99076040123d49868b05306d8870a2eac51f3c
DAFRA117 <99152899+DAFRA117@users.noreply.github.com>
2022-02-20 21:45:05 -0600

    Agregando las katas

---
+++ CursoPython/Kata1.py
@@ -0,0 +1,35 @@
+# program.py
+from unicodedata import name
+from numpy import product
+sum = 1 + 3 
+print(sum)
+#Saludos desde la consola 
+print('Buenos días alegría')
+sum2 = 1 + 2 # 3
+product =sum * 2
+print(product)
+print(sum * product)
+sum = 2 + 4 
+product = sum2 + 2
+print(product)
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+type(distancia_a_alfa_centauri)
+
+# # Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+print('Buenos días alegrÍa')
+name = input('introduzca su apodo')
+print("saludos:" + name)
+
+
